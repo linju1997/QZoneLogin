6983faff53ae6d831d6618e9e41743d3a9bb18ba
qiao <2218890669@qq.com>
2018-06-03 15:01:28 +0800

    修改README

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # QZoneLogin
 java实现，模拟QQ空间登录，获取QQCookie
 主要获取skey cookie，如需获取其他cookie，请自行修改
+````java
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String qqNumber = "";
+        String password = "";
+        QQClient qq = new QQClient(qqNumber,password);
+        boolean login = qq.loginQQ();
+        if (login) {
+            System.out.println("登陆成功");
+            qq.getCookie().forEach((x, y) -> System.out.println(x + ":" + y));
+        } else {
+            System.out.println("登陆失败");
+        }
+    }
+}
+````
--- src/main/java/QQClient/Main.java
+++ src/main/java/QQClient/Main.java
@@ -6,13 +6,13 @@ package QQClient;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        QQClient qq = new QQClient("2558807914", "QIAO520...");
-        boolean b = qq.loginQQ();
-        if (b) {
+        String qqNumber = "";
+        String password = "";
+        QQClient qq = new QQClient(qqNumber,password);
+        boolean login = qq.loginQQ();
+        if (login) {
             System.out.println("登陆成功");
-            qq.getCookie().forEach((x, y) -> {
-                System.out.println(x + ":" + y);
-            });
+            qq.getCookie().forEach((x, y) -> System.out.println(x + ":" + y));
         } else {
             System.out.println("登陆失败");
         }
